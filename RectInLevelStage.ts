e040db48fc7abdaf0efb4e1d2630437ff159b2f2
Anwesh43 <anweshthecool0@gmail.com>
2018-10-08 10:55:55 +0530

    adding RILNode class

--- RectInLevelStage.ts
+++ RectInLevelStage.ts
@@ -72,3 +72,44 @@ class Animator {
         }
     }
 }
+
+class RILNode {
+    prev : RILNode
+    next : RILNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new RILNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : RILNode {
+        var curr = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
